78720afdf21bcd946474ab1a247c7d25d34b075f
matssonalex <alexmatsson@gmail.com>
2022-05-11 15:38:01 +0200

    GAN.py update

--- GAN.py
+++ GAN.py
@@ -4,53 +4,31 @@ layers = tf.keras.layers
 
 
 class GAN(tf.keras.Model):
-    def __init__(self, input_dim=64, input_channels=3):
+    def __init__(self, input_dim=64, input_channels=1):
         super(GAN, self).__init__()
         
         self.generator = self.create_generator(input_dim, input_channels)    
-        
-        #self.generator.compile()
+
         self.generator.compile(optimizer=tf.keras.optimizers.Adam(),
                   loss="sparse_categorical_crossentropy",
                   metrics="accuracy")   # TODO CHANGE
      
 
-    # def double_conv_block(self, x, n_filters):
-    #     x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
-    #     x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
+    def call(self, inputs, training=None, mask=None): # TODO
+        return self.generator(inputs)
 
-    #     return x
-
-
-    # def downsample_block(self, x, n_filters):
-    #     skip_vec = self.double_conv_block(x, n_filters)
-    #     x = layers.MaxPool2D(2)(skip_vec)
-    #     # x = layers.Dropout(0.3)(x)
-        
-    #     return x, skip_vec
-
-    # def upsample_block(self, x, skip_vec, n_filters):
-    #     x = layers.Conv2DTranspose(n_filters, kernel_size=3, strides=2, padding='same')(x)
-    #     x = layers.concatenate([x, skip_vec])
-    #     # x = layers.Dropout(0.3)(x)
-    #     x = self.double_conv_block(x, n_filters)
-
-    #     return x
 
     def create_generator(self, input_dim, input_channels):
         filters = [16, 32, 64, 128, 256, 512]
         skip_vec = []
-        input = tf.keras.Input(shape=(input_dim, input_dim, 1))
-
-        
+        input = tf.keras.Input(shape=(input_dim, input_dim, input_channels))
 
         x = input    
         for n_filters in filters:
             x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
             x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
-            #x = self.double_conv_block(x, n_filters)
             skip_vec.append(x)
-            x = layers.MaxPool2D(2)(x)
+            x = layers.MaxPooling2D(2)(x)
 
         x = layers.Conv2D(1024, kernel_size=3, strides=1, padding='same', activation='relu')(x) # TODO RESNET?
         x = layers.Conv2D(1024, kernel_size=3, strides=1, padding='same', activation='relu')(x)
@@ -61,32 +39,19 @@ class GAN(tf.keras.Model):
             x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
             x = layers.Conv2D(n_filters, kernel_size=3, strides=1, padding='same', activation='relu')(x)
 
-            # x = self.double_conv_block(x, n_filters)
-            # self.skip_vec.append(x)
-            # x = layers.MaxPool2D(2)(x) 
-        #print(np.shape(input))
-        # x, skip_vec1 = self.downsample_block(input, 16),
-        # x, skip_vec2 = self.downsample_block(x, 32),
-        # x, skip_vec3 = self.downsample_block(x, 64),
-        # x, skip_vec4 = self.downsample_block(x, 128),
-        # x, skip_vec5 = self.downsample_block(x, 256),
-        # x, skip_vec6 = self.downsample_block(x, 512),
 
-        # x = self.double_conv_block(x, 1024),
-
-        # x = self.upsample_block(x, skip_vec6, 512),
-        # x = self.upsample_block(x, skip_vec5, 256),
-        # x = self.upsample_block(x, skip_vec4, 128),
-        # x = self.upsample_block(x, skip_vec3, 64),
-        # x = self.upsample_block(x, skip_vec2, 32),
-        # x = self.upsample_block(x, skip_vec1, 16)
-
-        output = layers.Conv2D(16, kernel_size=1, padding="same", activation = "relu")(x) #check activation here
+        output = layers.Conv2D(16, kernel_size=1, padding="same", activation = "softmax")(x) #check activation here
         
         model = tf.keras.Model(input, output)
         return model
    
 
-mod = GAN()
+mod = GAN(64)
 print(mod.generator.summary())
+
+# test_input = tf.random.normal([64, 64, 1])
+# # test_input = np.reshape(test_input, (None, 256, 256, 1))
+# print(np.shape(test_input))
+# gen_img = mod.generator.predict(test_input)
+
 
